9ae3a8b2406482e2b47b63aa3996c9546ff3fc7a
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 12:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+
+let input1 = prompt("Enter First Input ");
+let input2 = prompt("Enter Second Input ");
+
+
+	const num1 = parseInt(Math.abs(input1));
+	const num2 = parseInt(Math.abs(input2));
+
+	let result = (num1 * 2) + num2; 
+
+alert(`The final result is: ${result}`);
+
